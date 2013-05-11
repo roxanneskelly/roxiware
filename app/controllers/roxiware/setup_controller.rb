869c865791877cc92a90b6658655f8b35d32080a
Roxanne Skelly <roxanne@roxiware.com>
2013-05-11 05:15:33 -0700

    make sure setup is run aftr first template choose

--- app/controllers/roxiware/setup_controller.rb
+++ app/controllers/roxiware/setup_controller.rb
@@ -522,6 +522,9 @@ class Roxiware::SetupController < ApplicationController
 		    Roxiware::Param::Param.set_application_param("system", "current_template", "B8A73EF2-9C65-4022-ABD3-2D4063827108", params[:template_guid])
 		    Roxiware::Param::Param.set_application_param("system", "layout_scheme", "99FA5423-147C-4929-A432-268BDED6DE44", params[:template_scheme])
 		    refresh_layout
+		    @current_template = params[:template_guid]
+		    @layout_scheme = params[:template_scheme]
+		    load_layout
 		    run_layout_setup
 		    result = _set_setup_step("complete")
 		end
--- lib/roxiware/version.rb
+++ lib/roxiware/version.rb
@@ -1,3 +1,3 @@
 module Roxiware
-  VERSION = "0.2.78"
+  VERSION = "0.2.79"
 end

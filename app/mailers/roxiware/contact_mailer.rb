c0300266f7594e112589e84d3753923413b126aa
Roxanne Skelly <roxanne@roxiware.com>
2013-07-01 15:06:34 -0700

    RWE-16 Contact Form Response/Notification doesn't deal with embedded ruby well, sanitizer messes with it, as does editor.
    now you enter a variable name preceeded by a $ to access form data ($email).
    Added a contact page to the basic author template as well

--- app/mailers/roxiware/contact_mailer.rb
+++ app/mailers/roxiware/contact_mailer.rb
@@ -3,8 +3,12 @@ module Roxiware
 	default :from => "info@roxiware.com"
 
 	def contact_mailer(mailer, to, subject, params)
+	    mailer.gsub!(/\$([\w_]+|)/, '<%= \1 %>')
+            subject.gsub!(/\$([\w_]+|)/, '%{\1}')
 	    params[:from]  ||= Roxiware::Param::Param.application_param_val("system", "webmaster_email")
-	        format.html { render :inline=>mailer, :locals=>{:params=>params}}
+	    params[:site_title] = Roxiware::Param::Param.application_param_val("system", "title")
+	    mail(:to => to, :subject => subject % params, :from=>"\" #{params[:site_title]} \" < #{params[:from]} >") do |format|
+	        format.html { render :inline=>mailer, :locals=>params}
 	    end
 	end
     end
